a3b9d675cbe369ea524e4703dcca150b2560a4b0
dhirajkarangale <dakarangale02@gmail.com>
2024-11-19 22:51:13 +0530

    Project added

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const SocketServer = require('./socketServer');
+const corsOptions = {
+  credentials: true,
+  origin: 'http://localhost:3000',
+};
+const app = express();
+
+app.use(express.json())
+app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
+app.use(cookieParser())
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+  SocketServer(socket);
+})
+
+app.use('/api', require('./routes/authRouter'));
+app.use('/api', require('./routes/userRouter'));
+app.use('/api', require('./routes/postRouter'));
+app.use('/api', require('./routes/commentRouter'));
+app.use('/api', require('./routes/adminRouter'));
+app.use('/api', require('./routes/notifyRouter'));
+app.use('/api', require('./routes/messageRouter'));
+
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, err => {
+  if (err) throw err;
+  console.log("Database Connected!!")
+})
+
+const port = process.env.PORT || 8080;
+http.listen(port, () => { console.log("Listening on ", port); });
